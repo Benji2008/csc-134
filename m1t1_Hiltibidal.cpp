1e75b6c58ddbcde39a6d1b55059b6486203858cc
Benji2008 <hiltibic4869@student.faytechcc.edu>
2026-01-14 18:33:37 +0000

    finished m1c1

--- m1t1_Hiltibidal.cpp
+++ m1t1_Hiltibidal.cpp
@@ -2,6 +2,7 @@
 // M1T1 MODULE 1
 
 #include <iostream> 
+#include <cmath>
 using namespace std;
 
 int main() {
@@ -13,7 +14,7 @@ int main() {
     cin >> name; // where we actually get the input from user
     
     cout << "Hello " + name; + "welcome to c++";
-    cout << end1; // blank
+    cout << endl; // blank
 
     // get some numbers in there and do some math
     int num1;
@@ -21,19 +22,18 @@ int main() {
     cout << "enter a whole number: "; 
     cin >> num1; 
 
-    cout << "your number is " + to_string(num1;) << endl;
-    cout endl;
+    cout << "your number is " + to_string(num1) << endl;
+    cout << endl;
     // another way to print int in a string
-    cout << "your number is " + << num1; 
-    cout endl;
+    cout << "your number is " << num1; 
+    cout << endl;
 
     //get exponent from first num
     int exponent;
     cout << "enter whole number for exponentiation";
-    cin >> exponent:
+    cin >> exponent;
     int result = pow(num1, exponent);
-    cout << num1 << " raised to the power of " << exponent << " is " result << endl;
+    cout << num1 << " raised to the power of " << exponent << " is " << result << endl;
     
-
     return 0; 
 }
