dab67744179169074f864c4786397422856d0edc
Benji2008 <hiltibic4869@student.faytechcc.edu>
2026-01-21 18:24:21 +0000

    changed

---
+++ M2T1_Hiltibidal.cpp
@@ -0,0 +1,38 @@
+//csc 134
+// M2t1 user store - sales
+//Connor Hiltibidal 
+//1/14/26
+/*
+were selling food - you decide what you wanna sell
+tell me how much you have
+how much each costs
+and what your total would be.
+if we have time we'll make it more interactive
+*/
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    string item_name;
+    int item_count;
+    int purchased;
+double price_each ;
+double total_price;
+
+
+cout << setprecision(2) << fixed;
+
+cout << "welcome to our " << item_name << " store. " << endl;
+cout << "we have" << item_count << " " << item_name << "," << endl;
+cout << "they cost $" << price_each << " each. " << endl; 
+
+cout << "how many would you like to purchase?" ;
+cin >> purchased;
+
+total_price = purchased * price_each;
+
+cout << "Total price is $" << total_price << endl;
+
+return 0; 
+}
