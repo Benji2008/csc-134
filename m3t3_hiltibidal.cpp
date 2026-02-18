80b4971fa3671535b2de373ddbaf6f7d86ad08f7
Benji2008 <hiltibic4869@student.faytechcc.edu>
2026-02-18 18:27:21 +0000

    start numbers n crap

---
+++ m3t3_hiltibidal.cpp
@@ -0,0 +1,18 @@
+/*
+CSC 134 
+m3t3 - craps and numbers 
+connor h
+2/18/26
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using name space std;
+
+int mani(){
+
+
+    return 0;
+}
