a1cae193578fca67abc4e2401ce1e92b9dff1230
Benji2008 <hiltibic4869@student.faytechcc.edu>
2026-02-18 19:04:18 +0000

    basic craps works

--- m3t3_hiltibidal.cpp
+++ m3t3_hiltibidal.cpp
@@ -9,10 +9,45 @@ connor h
 #include <cstdlib>
 #include <ctime>
 
-using name space std;
+using namespace std;
+string status;
+int roll();
 
-int mani(){
+int main(){
+    //seed
+    srand(time(0));
+    //set up
+    int roll1, roll2, total, point;
+    string stattus;
+
+    roll1 = roll();
+    roll2 = roll();
+    total = roll1 + roll2;
+
+    // print the dice roll
+    cout << "Roll is: 🎲 " << roll1 << " + " << roll2 << " = " << total << endl;
+
+    //determine win or loss
+    if (total == 7 || total == 11) {
+        status = "win";
+    }
+    else if (total == 2 || total == 3 || total == 12) {
+        status = "lose";
+    
+    }
+    else {
+        // point
+        point = total; // save for later
+        status = "point";
+    }
+
+    cout << "Roll results; " << status << endl;
 
 
     return 0;
+}
+
+int roll() {
+    int num = (rand() % 6) + 1;
+    return num;
 }
