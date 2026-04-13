969f4565c32504787535ef0c76f3f6426b94e537
Benji2008 <hiltibic4869@student.faytechcc.edu>
2026-04-13 18:09:51 +0000

    working

--- M6T1.cpp
+++ M6T1.cpp
@@ -8,12 +8,16 @@ xp then stats
 #include <iostream>
 using namespace std;
 
+//array
+ void barChart(int xp[], int floors);
+
 int main() {
    const int STATS = 5;
     int xp[STATS];
     double total = 0.0;
     double average;
     int max = 0;
+   
     //input
     cout << "Dungean Xp Tracker" << endl;
     for (int i = 0; i < STATS; i++) {
@@ -38,6 +42,18 @@ int main() {
     cout << "Total XP: " << total << endl;
     cout << "Average XP: " << average << endl;
     cout << "Best XP: " << max << endl;
+    barChart(xp, STATS);
 
     return 0;
+}
+
+void barChart(int xp[], int floors) {
+    const int SCALE = 10;  // 1 bar = 10 XP
+    for (int i = 0; i < floors; i++) {
+        cout << "F" << (i+1) << " | ";
+        for (int b = 0; b < xp[i]/SCALE; b++) {
+            cout << "█";
+        }
+        cout << " " << xp[i] << "\n";
+    }
 }
